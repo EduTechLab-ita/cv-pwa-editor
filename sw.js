3af874832ae28bff3ee3920c4827882688256aba
Fabio Rizzotto - EduTechLab <edutechlab.ita@gmail.com>
2026-05-09 15:01:58 +0200

    v5.8.8: campo link+testo in esperienze, istruzione e formatore (tutti i template)

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 // Service Worker per VitaeLab - EduTechLab Italia
 // Per forzare aggiornamenti PWA: incrementare i numeri di versione qui sotto
 
-const CACHE_NAME = 'vitaelab-v5.8.7';
-const STATIC_CACHE = 'vitaelab-static-v5.8.7';
-const DYNAMIC_CACHE = 'vitaelab-dynamic-v5.8.7';
+const CACHE_NAME = 'vitaelab-v5.8.8';
+const STATIC_CACHE = 'vitaelab-static-v5.8.8';
+const DYNAMIC_CACHE = 'vitaelab-dynamic-v5.8.8';
 
 // File da cachare immediatamente (cache statica)
 const STATIC_FILES = [

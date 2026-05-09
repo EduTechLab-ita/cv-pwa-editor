e6678f57ae636bd706470618761ed34977b5edc3
Fabio Rizzotto - EduTechLab <edutechlab.ita@gmail.com>
2026-05-09 14:41:19 +0200

    v5.8.6: fix doppio contatore stampa, testo giustificato, privacy sezione separata

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 // Service Worker per VitaeLab - EduTechLab Italia
 // Per forzare aggiornamenti PWA: incrementare i numeri di versione qui sotto
 
-const CACHE_NAME = 'vitaelab-v5.8.5';
-const STATIC_CACHE = 'vitaelab-static-v5.8.5';
-const DYNAMIC_CACHE = 'vitaelab-dynamic-v5.8.5';
+const CACHE_NAME = 'vitaelab-v5.8.6';
+const STATIC_CACHE = 'vitaelab-static-v5.8.6';
+const DYNAMIC_CACHE = 'vitaelab-dynamic-v5.8.6';
 
 // File da cachare immediatamente (cache statica)
 const STATIC_FILES = [
